f0c54fc61fcd1e6dd57f24e8a9f74d3d69fb52f7
xuning <xningdd@163.com>
2025-01-31 14:49:32 +0800

    update

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -10,19 +10,23 @@
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(&data);
 
-    string_uppercase(&data);
+    string_uppercase(data);
 }
 
 // Should not take ownership
-fn get_char(data: String) -> char {
-    data.chars().last().unwrap()
+fn get_char(data: &String) -> char {
+    if let Some(c) = data.chars().last() {
+        c
+    } else {
+        '0'
+    }
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+fn string_uppercase(mut data: String) {
+    data = data.to_uppercase();
 
     println!("{}", data);
 }
